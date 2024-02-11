7a91e1ffcea1e931d961dfd6cd217eaf0b6655b9
sp-droid <52839915+sp-droid@users.noreply.github.com>
2024-02-11 21:32:55 +0100

    codelab colorful grid

---
+++ Projects/JavaScript/5codelabWebGPU/3drawGrid/default.wgsl
@@ -0,0 +1,35 @@
+struct VertexInput {
+    @location(0) pos: vec2f,
+    @builtin(instance_index) instance: u32
+}
+
+struct VertexOutput {
+    @builtin(position) pos: vec4f,
+    @location(0) cell: vec2f
+}
+
+@group(0) @binding(0) var<uniform> grid: vec2f;
+
+@vertex
+fn vertexMain (input: VertexInput) -> VertexOutput {
+    
+    let i = f32(input.instance);
+    let cell = vec2f(i % grid.x, floor(i / grid.x));
+
+    let gridPos = (input.pos + 1 + cell*2) / grid - 1;
+
+    var output: VertexOutput;
+    output.pos = vec4f(gridPos, 0, 1);
+    output.cell = cell;
+    return output;
+}
+
+// struct FragInput {
+//     @location(0) cell: vec2f
+// }
+
+@fragment
+fn fragmentMain (input: VertexOutput) -> @location(0) vec4f {
+    let c = input.cell / grid;
+    return vec4f(c, 1-c.x, 1);
+}
---
+++ Projects/JavaScript/5codelabWebGPU/3drawGrid/index.js
@@ -0,0 +1,132 @@
+// Constants
+const GRID_SIZE = 32;
+
+// Checking WebGPU browser support
+if (!navigator.gpu) {
+    throw new Error("WebGPU not supported on this browser.");
+} else { console.log("WebGPU supported.") }
+
+// Requesting GPU adapter
+const adapter = await navigator.gpu.requestAdapter();
+if (!adapter) { throw new Error("No appropriate GPUAdapter found.") }
+// Requesting GPU device
+const device = await adapter.requestDevice();
+
+// Requesting GPU canvas context
+const canvas = document.querySelector("canvas");
+const context = canvas.getContext('webgpu');
+const canvasFormat = navigator.gpu.getPreferredCanvasFormat();
+context.configure({
+    device: device,
+    format: canvasFormat
+});
+
+// Defining a square as two triangles
+const pos = 0.90;
+const vertices = new Float32Array([
+    -pos, -pos,
+     pos, -pos,
+     pos,  pos,
+
+    -pos, -pos,
+     pos,  pos,
+    -pos,  pos
+]);
+// Allocating GPU memory for the vertices
+const vertexBuffer = device.createBuffer({
+    label: "Cell vertices", // Buffer label
+    size: vertices.byteLength, // Buffer byte size
+    // (32bit = 4 bytes) 6 2D-vertices = 12 float32 = 12 * 4 bytes = 48 bytes
+    usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST // two flags, one to declare
+    // it's going to be used for vertex data and the other to be able to copy data into it
+});
+// Writing GPU memory
+device.queue.writeBuffer(vertexBuffer, /*bufferOffset=*/0, vertices);
+// Defining the structure of the GPU memory object.
+const vertexBufferLayout = {
+    arrayStride: 8, // Vertex byte size. float32 * 2 = 8 bytes
+    attributes: [{
+        format: "float32x2", // Vertex byte structure
+        offset: 0, // After how many bytes does the attribute starts
+        shaderLocation: 0 // Unique int between 0 and 15
+    }]
+};
+
+// Allocating GPU memory for the gridsize
+const uniformArray = new Float32Array([GRID_SIZE, GRID_SIZE]);
+const uniformBuffer = device.createBuffer({
+    label: "Grid uniforms",
+    size: uniformArray.byteLength,
+    usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST
+});
+device.queue.writeBuffer(uniformBuffer, 0, uniformArray);
+
+// Shaders
+const cellShaderModule = device.createShaderModule({
+    label: "Cell shader",
+    code: await loadTextFile("default.wgsl")
+});
+// Create render pipeline
+const cellPipeline = device.createRenderPipeline({
+    label: "Cell pipeline",
+    layout: "auto",
+    vertex: {
+        module: cellShaderModule,
+        entryPoint: "vertexMain",
+        buffers: [vertexBufferLayout]
+    },
+    fragment: {
+        module: cellShaderModule,
+        entryPoint: "fragmentMain",
+        targets: [{
+            format: canvasFormat
+        }]
+    }
+});
+
+// Create a bind group
+const bindGroup = device.createBindGroup({
+    label: "Cell renderer bind group",
+    layout: cellPipeline.getBindGroupLayout(0),
+    entries: [{
+        binding: 0,
+        resource: { buffer: uniformBuffer }
+    }]
+});
+
+// Encoder that sends instructions to the GPU
+const encoder = device.createCommandEncoder();
+
+// Render pass
+const pass = encoder.beginRenderPass({
+    colorAttachments: [{
+        view: context.getCurrentTexture().createView(),
+        loadOp: "clear",
+        clearValue: [0,0,0.4,1],
+        storeOp: "store"
+    }]
+});
+
+pass.setPipeline(cellPipeline);
+pass.setVertexBuffer(0, vertexBuffer);
+
+pass.setBindGroup(0, bindGroup);
+
+pass.draw(vertices.length / 2, GRID_SIZE*GRID_SIZE); // 6 vertices
+
+pass.end();
+
+// Create a command buffer and submit it to the queue of the GPU device
+device.queue.submit([encoder.finish()]);
+
+// Functions
+async function loadTextFile(filePath) {
+    try {
+        const response = await fetch(filePath);
+        if (!response.ok) { throw new Error('Network response was not ok') }
+        const text = await response.text();
+        return text;
+    } catch (error) {
+        console.error('There was a problem fetching the file:', error);
+    }
+}
