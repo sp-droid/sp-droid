16735a7059748ce71b3cf6de2620cd03743d8088
sp-droid <52839915+sp-droid@users.noreply.github.com>
2024-03-09 17:20:08 +0100

    Remove the typescript angular course

--- Projects/TypeScript/1udemyAngular/2courseProject/src/app/shared/ingredient.model.ts
+++
@@ -1,4 +0,0 @@
-export class Ingredient {
-    // This is a short way of first declaring the public variables, then assigning them in the constructor
-    constructor(public name: string, public amount: number) {}
-}
