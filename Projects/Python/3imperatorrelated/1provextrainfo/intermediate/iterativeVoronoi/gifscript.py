0a461c4588f0636473e765bde31d68a7bc03fd4a
sp-droid <52839915+sp-droid@users.noreply.github.com>
2024-03-31 23:06:17 +0200

    Iterative voronoi massive improvements in speed and solved bugs

--- Projects/Python/3imperatorrelated/1provextrainfo/intermediate/iterativeVoronoi/gifscript.py
+++ Projects/Python/3imperatorrelated/1provextrainfo/intermediate/iterativeVoronoi/gifscript.py
@@ -6,16 +6,16 @@ from joblib import Parallel, delayed
 
 ## OUTPUT OPTIONS
 name = 'europe'
-delay = 100 #in ms
-endDelay = 2000 #in ms
-leftTopRightBottom = (12600,4800,16300,7400)
-horizontalSize = 1920
+delay = 1 #in ms
+endDelay = 0 #in ms
+leftTopRightBottom = (7352,854,10287,2504)
+horizontalSize = 600
 ## OUTPUT OPTIONS
 
 aspectRatio = (leftTopRightBottom[2]-leftTopRightBottom[0])/(leftTopRightBottom[3]-leftTopRightBottom[1])
-sizes = (horizontalSize, int(horizontalSize/aspectRatio))
+sizes = (horizontalSize, 340)
 
-filenames = [file for file in os.listdir('steps') if file.endswith('png')]
+filenames = [file for i, file in enumerate(os.listdir('steps')) if file.endswith('png') and i%4==0 ]
 
 def loadImage(filename):
     Image.MAX_IMAGE_PIXELS = None
@@ -24,7 +24,8 @@ images = Parallel(n_jobs=-1)(delayed(loadImage)(filename) for filename in tqdm(f
 
 Image.MAX_IMAGE_PIXELS = None
 image = Image.open('steps/'+filenames[-1]).crop(leftTopRightBottom).resize(sizes, Image.Resampling.LANCZOS)
+image.save('thumbnail.jpg')
 for _ in range(int(endDelay/delay)):
     images.append(image)
 
-images[0].save(f'{name}.avif', quality=70, save_all=True, optimize=False, append_images=images[1:], duration=delay)
+images[0].save(f'{name}.avif', quality=80, save_all=True, optimize=False, append_images=images[1:], duration=delay)
