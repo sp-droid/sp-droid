0a08ad861b69645ada934ca5b0a42f3eba55a417
sp-droid <52839915+sp-droid@users.noreply.github.com>
2024-03-31 14:08:47 +0200

    Greatly improved the gifscript in speed and compression

--- Projects/Python/3imperatorrelated/1provextrainfo/intermediate/imgs/gifscript.py
+++ Projects/Python/3imperatorrelated/1provextrainfo/intermediate/imgs/gifscript.py
@@ -1,31 +1,34 @@
 import os
-import numpy as np
-import imageio.v2 as imageio
+import pillow_avif
 from PIL import Image
 from tqdm import tqdm
-
-Image.MAX_IMAGE_PIXELS = None
+from joblib import Parallel, delayed
 
 ## OUTPUT OPTIONS
 name = 'europe'
-delay = 500 #in ms
-loop = 4
+delay = 100 #in ms
+endDelay = 2000 #in ms
 leftTopRightBottom = (12600,4800,16300,7400)
 horizontalSize = 1920
-##
+## OUTPUT OPTIONS
 
 aspectRatio = (leftTopRightBottom[2]-leftTopRightBottom[0])/(leftTopRightBottom[3]-leftTopRightBottom[1])
 sizes = (horizontalSize, int(horizontalSize/aspectRatio))
 
 filenames = [file for file in os.listdir() if file.endswith('png')]
 
-images = []
-for filename in tqdm(filenames):
-    image = imageio.imread(filename)
+# images = []
+# for filename in tqdm(filenames):
+#     image = Image.open(filename).crop(leftTopRightBottom).resize(sizes, Image.Resampling.LANCZOS)
+#     images.append(image)
+def loadImage(filename):
+    Image.MAX_IMAGE_PIXELS = None
+    return Image.open(filename).crop(leftTopRightBottom).resize(sizes, Image.Resampling.LANCZOS)
+images = Parallel(n_jobs=-1)(delayed(loadImage)(filename) for filename in tqdm(filenames))
 
-    image = Image.fromarray(image).crop(leftTopRightBottom).resize(sizes, Image.Resampling.LANCZOS)
-    images.append(np.array(image, dtype=np.uint8))
-for _ in range(5):
-    images.append(np.array(image, dtype=np.uint8))
+Image.MAX_IMAGE_PIXELS = None
+image = Image.open(filenames[-1]).crop(leftTopRightBottom).resize(sizes, Image.Resampling.LANCZOS)
+for _ in range(int(endDelay/delay)):
+    images.append(image)
 
-imageio.mimsave('europe.gif', images, format='GIF', loop=loop, duration=delay)
+images[0].save(f'{name}.avif', quality=70, save_all=True, optimize=False, append_images=images[1:], duration=delay)
