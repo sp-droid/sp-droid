eca1c8e6b5c54e109d7e079deb7e361760efdd46
sp-droid <52839915+sp-droid@users.noreply.github.com>
2024-03-31 21:36:58 +0200

    iterative voronoi folder structure change 2

---
+++ Projects/Python/3imperatorrelated/1provextrainfo/intermediate/iterativeVoronoi/gifscript.py
@@ -0,0 +1,30 @@
+import os
+import pillow_avif
+from PIL import Image
+from tqdm import tqdm
+from joblib import Parallel, delayed
+
+## OUTPUT OPTIONS
+name = 'europe'
+delay = 100 #in ms
+endDelay = 2000 #in ms
+leftTopRightBottom = (12600,4800,16300,7400)
+horizontalSize = 1920
+## OUTPUT OPTIONS
+
+aspectRatio = (leftTopRightBottom[2]-leftTopRightBottom[0])/(leftTopRightBottom[3]-leftTopRightBottom[1])
+sizes = (horizontalSize, int(horizontalSize/aspectRatio))
+
+filenames = [file for file in os.listdir('steps') if file.endswith('png')]
+
+def loadImage(filename):
+    Image.MAX_IMAGE_PIXELS = None
+    return Image.open('steps/'+filename).crop(leftTopRightBottom).resize(sizes, Image.Resampling.LANCZOS)
+images = Parallel(n_jobs=-1)(delayed(loadImage)(filename) for filename in tqdm(filenames))
+
+Image.MAX_IMAGE_PIXELS = None
+image = Image.open('steps/'+filenames[-1]).crop(leftTopRightBottom).resize(sizes, Image.Resampling.LANCZOS)
+for _ in range(int(endDelay/delay)):
+    images.append(image)
+
+images[0].save(f'{name}.avif', quality=70, save_all=True, optimize=False, append_images=images[1:], duration=delay)
