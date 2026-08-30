97298af48f71d92243955ab00849f9247928509e
sp-droid <52839915+sp-droid@users.noreply.github.com>
2026-08-30 18:47:33 +0200

    prediction trainer removed

--- Projects/Zig/2predictionTrainer/src/main.zig
+++
@@ -1,8 +0,0 @@
-const std = @import("std");
-
-pub fn main(init: std.process.Init) !void {
-    @import("game.zig").run(init) catch |err| {
-        std.log.err("Pre-stroke labeler stopped: {s}", .{@errorName(err)});
-        return err;
-    };
-}
